5ec63a90e48704a083ffdd191aae3748277cbba9
MatthewRBevins <47047931+MatthewRBevins@users.noreply.github.com>
2023-10-19 07:50:02 -0700

    backlog

--- day19.py
+++ day19.py
@@ -1,5 +1,16 @@
-costs = [[1, 5, 2], [2, 3, 1], [7, 3, 5], [6, 2, 3]]
-f = []
-for i in costs:
-    f.append(min(i))
-print(f)
+# row = house in row, column = color, value = cost
+costs = [[1, 5, 2], [2, 3, 1], [7, 3, 5], [6, 3, 2]]
+
+def find_best_cost(cost, level, prevIndex):
+    if level == len(costs):
+        return cost
+    bestCost = 1000
+    for i in range(len(costs[level])):
+        if i != prevIndex:
+            currentCost = find_best_cost(cost+costs[level][i], level+1, i)
+            if currentCost < bestCost:
+                bestCost = currentCost
+    return bestCost
+
+print(find_best_cost(0, 0, -1))
+    
--- day23.py
+++ day23.py
@@ -1,9 +1,65 @@
-board = [[False,False,False,False],[True,True,False,True],[False,False,False,False],[False,False,False,False]]
+board = [[False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False],
+         [False,False,False,False,False,False,False,False,False,False,False,False]]
+# first number is row number
 start = [3,0]
 end = [0,0]
-while start != end:
-    if start[0] > end[0] and board[start[0]-1][start[1]:
-        start[0] -= 1
-    elif start[0] < end[0] and board[start[0]+1][start[1]:
-        start[0] += 1
-    elif s
+def genPossibleMoves(pos, prevMoves):
+    moves = []
+    # up
+    if pos[0]-1 > -1:
+        moves.append([pos[0]-1, pos[1]])
+    # down
+    if pos[0]+1 < len(board):
+        moves.append([pos[0]+1, pos[1]])
+    # right
+    if pos[1]+1 < len(board[0]):
+        moves.append([pos[0], pos[1]+1])
+    # left
+    if pos[1]-1 > -1:
+        moves.append([pos[0], pos[1]-1])
+    goodMoves = []
+    for i in range(len(moves)):
+        good = True
+        for j in prevMoves:
+            if (moves[i][0] == j[0] and moves[i][1] == j[1]) or board[moves[i][0]][moves[i][1]]:
+                good = False
+                break
+        if good:
+            goodMoves.append(moves[i])
+    return goodMoves
+
+def move(pos, moves, prevMoves):
+    prevMoves.append(pos)
+    possible = genPossibleMoves(pos, prevMoves)
+    if pos[0] == end[0] and pos[1] == end[1]:
+        return [prevMoves, moves]
+    if len(possible) == 0:
+        return None
+    bestMove = [[],len(board)*len(board[0])]
+    for i in possible:
+        cMove = move(i, moves+1, prevMoves[0:moves])
+        if cMove is not None and cMove[1] < bestMove[1]:
+            bestMove = cMove
+    return bestMove
+
+nav = move(start, 0, [])
+if len(nav[0]) == 0 and nav[1] > 0:
+    print("IMPOSSIBLE")
+else:
+    print("You can complete the maze in " + str(nav[1]) + " steps by using the path " + str(nav[0]))
