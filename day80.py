cff1036d59c48e7e1290dcf69257548d6264068d
MSGuy01 <47047931+MatthewRBevins@users.noreply.github.com>
2023-12-11 22:24:29 -0800

    finished day 80

--- day80.py
+++ day80.py
@@ -1,7 +1,23 @@
 class Node:
-    def __init__(val, l=None, r=None):
+    def __init__(self, val, left=None, right=None):
         self.val = val
-        self.l = l
-        self.r = r
-    def traverse(level):
-        
+        self.left = left
+        self.right = right
+    def traverse(self, layer):
+        biggestLayer = 0
+        biggestVal = 0
+        if self.left == None and self.right == None:
+            return [self.val, layer]
+        if self.left != None:
+            t = self.left.traverse(layer+1)
+            if t[1] > biggestLayer:
+                biggestVal = t[0]
+                biggestLayer = t[1]
+        if self.right != None:
+            t = self.right.traverse(layer+1)
+            if t[1] > biggestLayer:
+                biggestVal = t[0]
+                biggestLayer = t[1]
+        return [biggestVal, biggestLayer]
+a = Node(1,Node(2,Node(3)),Node(4))
+print(a.traverse(1))
