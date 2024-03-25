63a01e67c47543d0402050eec7c222b622a4e339
MatthewRBevins <47047931+MatthewRBevins@users.noreply.github.com>
2024-03-24 19:14:16 -0700

    Update day182.py

--- 181-190/day182.py
+++ 181-190/day182.py
@@ -8,10 +8,29 @@ adjacencyList = [
     [0, 2, 6],
     [0, 3, 5]
 ]
-def checkConnected(graph):
-    return
+def checkConnected(connections, total, done, list):
+    for i in connections:
+        if not i in done:
+            done.append(i)
+            n = checkConnected(list[i], total, done.copy(), list)
+            if n:
+                return True
+        if len(done) == total:
+            return True
+    return False
+m = False
 for i in range(len(adjacencyList)):
     for j in range(len(adjacencyList[i])):
+        removedEdge = adjacencyList.copy()
+        del removedEdge[i][j]
+        del removedEdge[j][removedEdge[j].index(removedEdge[i][j])]
+        if checkConnected(removedEdge[0], 7, [], removedEdge):
+            print("NOT MINIMALLY CONNECTED")
+            m = True
+            break
+    if m:
         break
+if not m:
+    print("MINIMALLY CONNECTED")
 # delete edge
 # check whether connected recursively
